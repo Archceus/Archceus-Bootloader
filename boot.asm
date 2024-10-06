17e4053550c54b99caec95ba6f6a4c5df7d56834
Archceus <vishalmalathi2005@gmail.com>
2024-10-06 23:07:06 +0530

    calls video interrupt

--- boot.asm
+++ boot.asm
@@ -14,3 +14,14 @@ clearscreen:
     PUSHA
 
     MOV AH, 0x07 ; tells BIOS to scroll down window
+    MOV AL, 0X00 ; clear entire window
+    MOV BH, 0x07 ; White on black
+    MOV CX, 0x00 ; specifies top left of screen as (0, 0)
+    MOV DH, 0x18 ; 18h = 24 rows of chars
+    MOV DL, 0x4f ; 4fh = 79 cols of chars
+    int 0x10     ; calls video interrupt
+
+    POPA
+    MOV SP, BP
+    POP BP
+    RET
